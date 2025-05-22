78cb764458fbd1f42449657ed9fa998c9840049d
sowmiyaalex <sowmiyaalexander25@gmail.com>
2025-05-22 15:24:42 +0530

    Add files via upload

---
+++ pointearr.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void reverse(int  *arr,int n){
+    int *start=arr,*end= arr+n-1;
+    while(end>start)
+    {
+        int t=*start;
+         *start=*end;
+         *end=t;
+        start++;
+        end--;
+    }
+}
+int main()
+{
+    int s[]={1,2,3,4,5,6};
+    int *str=s;
+    int n= sizeof(s)/sizeof (s[0]);
+    reverse(s,n);
+    for(int i=0;i<n;i++){
+        printf("%d ",*str);
+        str++;
+    }
+}
